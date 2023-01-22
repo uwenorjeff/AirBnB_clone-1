cf8b619b4f4485a9aa2ca1ba61db42dcc924c80b
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2023-01-22 11:44:50 +0100

    Create setup_mysql_test.sql

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- a script that prepares a MySQL server for the project
+-- create a database and set privileges for a new user
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
