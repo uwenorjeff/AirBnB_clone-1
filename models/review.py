98598320c8360045526bfd9917bfd6640700f499
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2023-02-05 18:26:59 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,19 +1,24 @@
-#!/usr/bin/python3
-"""
-Defines the Review class.
-"""
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" holds class Review"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
-    """Represent a review
+class Review(BaseModel, Base):
+    """Representation of Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
 
-    Attributes:
-        place_id (str): The Place id
-        user_id (str): The User id
-        text (str): The text of the review
-
-    """
-    place_id = ""
-    user_id = ""
-    text = ""
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
